274457dd9aa1ccbf24a31785ee0d9455005d7b96
PershikovaEP <zem524@mail.ru>
2022-12-21 00:26:16 +0300

    update tests

--- cypress/e2e/books.cy.js
+++ cypress/e2e/books.cy.js
@@ -29,43 +29,43 @@ it('Should successfully add books without cover and book file and added to favor
 //успешное перемещение уже добавленной книги в фавориты с главной страницы
 it('Should successfully move added books to favorites', () => {
   cy.addBook(
-    'Harry Potter And The Chamber of secrets',
+    'Harry Potter And The Chamber of secrets5',
     'The book tells about the adventures of a young English magician',
     'J.K. Rowling'
   );
 
-  //принудительный запуск, так как тярется родительский элемент DOM
-  cy.get('.btn-success').last().click({ force: true });
+  cy.contains('Harry Potter And The Chamber of secrets5')
+    .find('.btn-success')
+    .click();
   cy.contains('Favorites').click();
-  cy.contains('Harry Potter And The Chamber of secrets').should('be.visible');
+
+  cy.contains('Harry Potter And The Chamber of secrets5').should('be.visible');
 });
 
 //успешное удаление книги из фаворитов с главной страницы
 it('should successfully remove book from favorites', () => {
   cy.addBook(
-    'Harry Potter and the Prisoner of Azcoban',
+    'Harry Potter and the Prisoner of Azcoban5',
     'The book tells about the adventures of a young English magician',
     'J.K. Rowling',
     'yes'
   );
 
-  //удаляем последнюю книгу из фаворитов
-  //react перезагружает страницу и теряется родительский элемент DOM
-  /* cy.get('.mt-3:nth-last-child(2) .h-100 > .card-footer > .btn')
-    .last()
-    .wait(15000)
-    .click();*/
+  // cy.contains ищет первый элемент
+  cy.contains('Harry Potter and the Prisoner of Azcoban5')
+    .find('.btn-secondary')
+    .click();
 
-  cy.get('.mt-3:nth-last-child(2) .h-100 > .card-footer > .btn')
-    .last()
-    .should('be.visible')
-    .then((e) => {
-      Cypress.$(e).click();
-    });
+  // Если тесты мы запускаем несколько раз и соответственно книг
+  // с одним названием оказывается много,то берем последнюю добавленную книгу
+  //cy.get('a.mt-3').last().find('.btn-secondary').click({ force: true });
+  //Тогда появляется ошибка - Cypress requires elements be attached in the DOM to interact with them.
+  //теряется родительский элемент при выполнении find(), так как большая цепочка элементов,
+  //а react успевает перерисовать DOM за это время
 
-  //cy.contains('Harry Potter and the Prisoner of Azcoban').should('not.be.visible');
-  cy.get('.mt-3:nth-last-child(2) .h-100 > .card-footer > .btn').should(
-    'have.text',
-    'Add to favorite'
-  );
+  cy.contains('Harry Potter and the Prisoner of Azcoban5')
+    .find('button')
+    .should('have.text', 'Add to favorite');
+
+  //cy.get('a.mt-3').last().find('button').should('have.text', 'Add to favorite');
 });
